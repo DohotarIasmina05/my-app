116f1f09b6f3acd4329ece385539c29f09e9d90a
vivianaduda26 <viviana.duda@student.upt>
2021-04-21 09:27:15 +0300

    subtask VL-14

--- src/main/exceptions/InvalidLoginException.java
+++ src/main/exceptions/InvalidLoginException.java
@@ -3,6 +3,7 @@ package com.mycompany.exceptions;
 public class InvalidLoginException extends Exception{
 
     public InvalidLoginException(){
+
         super(String.format("Incorect login.Please try again"));
     }
 }
